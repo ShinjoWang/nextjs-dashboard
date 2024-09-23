961531e84c6cf0004bbff8072e5e4bf10ec8ae25
Shinjo Wang <dhkdtlswh@gmail.com>
2024-09-24 08:57:46 +0900

    [Chapter 6] :card_file_box: Setting Up Your Database

---
+++ app/lib/db.ts
@@ -0,0 +1,10 @@
+import pg from 'pg';
+
+const { Pool } = pg
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+})
+export default pool;
